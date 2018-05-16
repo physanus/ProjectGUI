df9db642cc96f2ffe334badfc6b0abda74869c13
physanus <physanus@users.noreply.github.com>
2018-05-16 08:46:55 +0200

    update

--- src/main/java/de/danielprinz/ProjectGUI/resources/SerializedCommands.java
+++ src/main/java/de/danielprinz/ProjectGUI/resources/SerializedCommands.java
@@ -1,18 +1,61 @@
 package de.danielprinz.ProjectGUI.resources;
 
+import de.danielprinz.ProjectGUI.exceptions.UnsupportedFileTypeException;
+
 import java.util.ArrayList;
 
 public class SerializedCommands {
 
     private ArrayList<Command> serializedCommands;
 
-    public SerializedCommands(ArrayList<Command> serializedCommands) {
-        this.serializedCommands = serializedCommands;
-    }
-
     public SerializedCommands() {
         this.serializedCommands = new ArrayList<>();
     }
+
+    public SerializedCommands(ArrayList<String> fileContent) throws UnsupportedFileTypeException {
+
+        this.serializedCommands = new ArrayList<>();
+        boolean error = false;
+
+        for(String line : fileContent) {
+            CommandType commandType;
+            if(line.startsWith("PD"))
+                commandType = CommandType.PD;
+            else if(line.startsWith("PU"))
+                commandType = CommandType.PU;
+            else if(line.startsWith("SP"))
+                // select pen, unused
+                continue;
+            else if(line.startsWith("IN"))
+                // initialize, unused
+                continue;
+            else {
+                error = true;
+                System.err.println("Found unknown command: " + line.substring(0, 2));
+                continue;
+            }
+
+            line = line.substring(2, line.length()); // remove command and semicolon
+
+            String[] split = line.split(",");
+            int x, y;
+            for(int i= 0; i < split.length; ) {
+                try {
+                    x = Integer.parseInt(split[i++]);
+                    y = Integer.parseInt(split[i++]);
+                } catch (NumberFormatException e) {
+                    throw new UnsupportedFileTypeException();
+                }
+
+                this.serializedCommands.add(new Command(commandType, x, y));
+            }
+        }
+
+        if(error)
+            throw new UnsupportedFileTypeException();
+    }
+
+
 
 
     public ArrayList<Command> getValues() {
